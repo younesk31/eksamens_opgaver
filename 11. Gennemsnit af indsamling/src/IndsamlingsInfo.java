af4dc2c6aabb739f9082af0fa6a1c4dcdc4932b8
younesk <71063287+younesk31@users.noreply.github.com>
2021-01-17 15:11:27 +0100

    update

--- 11. Gennemsnit af indsamling/src/IndsamlingsInfo.java
+++ 11. Gennemsnit af indsamling/src/IndsamlingsInfo.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class IndsamlingsInfo {
-    private int sumall = 0;
+    private int sumall;
 
     public void calculateAverage() throws IOException {
         File myFile = new File("file.txt");
         FileWriter myWriter = new FileWriter("Gennemsnit.txt");
         Scanner myFileReader = new Scanner(myFile);
-        while (myFileReader.hasNextLine()){
+        while (myFileReader.hasNextLine()) {
             String line = myFileReader.nextLine();
             String[] words = line.split("\\W+");
             int a;
@@ -20,20 +20,20 @@ public class IndsamlingsInfo {
                 a = Integer.parseInt(words[i]);
                 sum += a;
                 sumAll(a);
-                days = words.length-1;
+                days = words.length - 1;
             }
-            sum = sum/days;
-            System.out.println("Navn: "+words[0]+" - Gennemsnit: "+sum+" kr.");
-            String average = "Navn: "+words[0]+" - Gennemsnit: "+sum+" kr.";
-            myWriter.write(average+"\n");
+            sum = sum / days;
+            System.out.println("Navn: " + words[0] + " - Gennemsnit: " + sum + " kr.");
+            String average = "Navn: " + words[0] + " - Gennemsnit: " + sum + " kr.";
+            myWriter.write(average + "\n");
         }
         myFileReader.close();
         myWriter.close();
-        System.out.println("Indsamlet i alt af indsamlere: "+sumall+" kr.");
+        System.out.println("Indsamlet i alt af indsamlere: " + sumall + " kr.");
         //System.out.println("File - Gennemsnit.txt updated");
     }
 
-    public void sumAll(int sum){
+    public void sumAll(int sum) {
         sumall += sum;
     }
 }
